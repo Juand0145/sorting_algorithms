ef591306f01468c37df84dd7e3c3351d94ad0709
Juand0145 <juand0145@gmail.com>
2021-06-10 15:57:22 -0500

    Adding task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - is a simple sorting algorithm
+ * that builds the final sorted array (or list) one item at a time.
+ * @list: Pointer to the linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *node = (*list)->next;
+    listint_t *next_node = NULL;
+
+    if (!list || !(*list))
+        return;
+
+    while(node)
+    {
+        next_node = node->next;
+
+        while (node->prev && node->n < node->prev->n)
+        {
+            node->prev->next = node->next;
+
+            if (node->next)
+                node->next->prev = node->prev;
+
+            node->next = node->prev;  
+            node->prev = node->next->prev;
+            node->next->prev = node;
+
+            if (node->prev == NULL)
+                *list = node;
+
+            else
+                node->prev->next = node;
+
+            print_list(*list);
+        }
+        node = next_node;
+    }
+}
--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void print_list(const listint_t *list);
 
 /*Functions Prototypes*/
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif
