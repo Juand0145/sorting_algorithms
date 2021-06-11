9a3705140485298d491bd3f9d84a43429c00115a
py-cc <paolacarrero731@gmail.com>
2021-06-11 06:53:34 -0500

    add file to check

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * selection_sort -  selection sort is an in-place comparison sorting
+ * algorithm. It has an O(n2) time complexity,
+ * which makes it inefficient on large lists
+ * @array: pointer to array
+ * @size: size of array
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, swap, j, min;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1;  j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+
+		if (i != min)
+		{
+			swap = array[i];
+			array[i] = array[min];
+			array[min] = swap;
+			print_array(array, size);
+		}
+	}
+
+}
